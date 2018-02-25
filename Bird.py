fb49f52435aa378c70eef865f03800e08764baa4
Kenrick Hatton <Kenrick.hatton@gmail.com>
2018-02-25 13:33:56 +0000

    Bird Code for stream two plus fractions code

--- Bird.py
+++ Bird.py
@@ -3,8 +3,15 @@ class Bird:
         self._kind = kind
         self._call = call
 
+class Parrot(Bird):
+    def __init__(self):
+        Bird.__init__(self, 'Parrot', 'Kah!')
+        self.learned_phrases = set()
+    def learn_phrase(self, phrase):
+        self.learned_phrases.add(phrase)
     def do_call(self):
-        print 'a %s goes %s' % (self._kind, self._call)
+        Bird.do_call(self)
+        print '\n'.join(self.learned_phrases)
 
 
 owl = Bird('Owl', 'Twit Twoo!')
